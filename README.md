adfb4fb49b794ed286b47bf3f2a2589253a2ba3d
Giovanni <giovanni.dellapelle@aesys.tech>
2021-11-10 10:52:03 +0100

    Cleaning repo

--- README.md
+++
@@ -1,2 +0,0 @@
-# ng-rating
-Angular rating
--- ngx-rating/projects/ngx-rating-lib/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngx-rating-lib
- */
-
-export * from './lib/ngx-rating-lib.service';
-export * from './lib/ngx-rating-lib.component';
-export * from './lib/ngx-rating-lib.module';
