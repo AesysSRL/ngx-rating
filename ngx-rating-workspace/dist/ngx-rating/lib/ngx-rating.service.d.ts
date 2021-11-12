6a2740ae34e65544e24a823d140f23688c0a870e
ranautomationtool <paolo.deluca@gmail.com>
2021-11-12 10:02:42 +0100

    Clean up

--- ngx-rating-workspace/dist/ngx-rating/lib/ngx-rating.service.d.ts
+++
@@ -1,6 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class NgxRatingService {
-    constructor();
-    static ɵfac: i0.ɵɵFactoryDeclaration<NgxRatingService, never>;
-    static ɵprov: i0.ɵɵInjectableDeclaration<NgxRatingService>;
-}
--- ngx-rating-workspace/projects/ngx-rating/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngx-rating
- */
-
-export * from './lib/ngx-rating.service';
-export * from './lib/ngx-rating.component';
-export * from './lib/ngx-rating.module';
